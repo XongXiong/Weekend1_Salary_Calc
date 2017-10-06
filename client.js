501de21bcd806f6a33c3ab43b05c099d7f5a5239
Xong Xiong <xxiong3153@gmail.com>
2017-10-06 16:10:15 -0500

    completed employee constructor

--- client.js
+++ client.js
@@ -1,5 +1,19 @@
-console.log('script sourced');
+//Constructor for new employees
+var Employee = function(firstName, lastName, eID, jobTitle, annSal){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.eID = eID;
+  this.jobTitle = jobTitle;
+  this.annSal = annSal;
+};
+//Sample Employee
+var exampleEmployee = new Employee('John', 'Smith', 123456, 'President', 360000);
+//Employee array to hold new inputs
+var employees = [exampleEmployee];
+//mCost used to calculate monthly cost of all employees
+var mCost = 0;
 
+console.log(employees);
 $(document).ready(main);
 
 function main(){
